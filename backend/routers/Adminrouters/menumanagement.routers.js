c7854d8b768ce0d6f8ab484f11aebae4ab5a3206
code-srikar <official.srikar.work@gmail.com>
2024-07-11 22:18:00 +0530

    menu backend

--- backend/routers/Adminrouters/menumanagement.routers.js
+++ backend/routers/Adminrouters/menumanagement.routers.js
@@ -4,17 +4,16 @@ const menuItemModel = require('../../models/menuItem.model')
 const menuManagementRouter = express.Router()
 
 menuManagementRouter.post('/:id/menu', async (req, res) => {
-    //const { itemId, name, price, description, restaurantId } = req.body;
-    const { itemId, name, category, veg, price, description, restaurantId } = req.body;
+    const { itemId, name, price, description, restaurantId } = req.body;
     // console.log(req.body)
-    const menuItem = await menuItemModel.create({ itemId, name, category, veg, price, description, restaurantId })
+    const menuItem = await menuItemModel.create({ itemId, name, price, description, restaurantId })
     res.status(200).json(menuItem)
 })
 
 menuManagementRouter.put('/:id/menu/:itemId', async (req, res) => {
-    const { itemId, name, category, veg, price, description, restaurantId } = req.body;
+    const { name, price, description, restaurantId } = req.body;
     // console.log(req.params.id, req.params.itemId)
-    const menuItem = await menuItemModel.findOneAndUpdate({ itemId: req.params.itemId }, { name, price, category, veg, description, restaurantId }, { new: true })
+    const menuItem = await menuItemModel.findOneAndUpdate({ itemId: req.params.itemId }, { name, price, description, restaurantId }, { new: true })
     res.status(200).json(menuItem)
 })
 
